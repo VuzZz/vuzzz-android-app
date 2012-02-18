267dd88060753a686c545d320cc3a644f31ab99b
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-02-18 01:04:53 +0100

    Adding AA, bullshit code to start listening to address intents

--- src/com/project202/ShowNoteActivity.java
+++ src/com/project202/ShowNoteActivity.java
@@ -1,13 +1,24 @@
 package com.project202;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.googlecode.androidannotations.annotations.EActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+@EActivity(R.layout.show_note)
 public class ShowNoteActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = getIntent();
+		Uri data = intent.getData();
+
+		if (data != null) {
+			Toast.makeText(this, data.toString(), Toast.LENGTH_LONG).show();
+		}
+	}
 }
