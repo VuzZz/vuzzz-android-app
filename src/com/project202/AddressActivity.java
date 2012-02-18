aaaf32adaa95dac1bdd5cd845b7c8549d3cad136
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-02-18 18:22:31 +0100

    work on maps

--- src/com/project202/AddressActivity.java
+++ src/com/project202/AddressActivity.java
@@ -1,32 +1,64 @@
 package com.project202;
 
-import android.view.View;
-import android.widget.EditText;
+import java.util.List;
 
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
 import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.NoTitle;
+import com.googlecode.androidannotations.annotations.SystemService;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.address_map)
 @NoTitle
 public class AddressActivity extends MapActivity {
-	
+
 	@ViewById
 	MapView mapView;
-	
+
 	@ViewById
 	EditText addressEditText;
-	
+
 	@ViewById
 	View addressHint;
-	
+
 	@ViewById
 	View searchButton;
-	
-	
+
+	@SystemService
+	InputMethodManager inputMethodManager;
+
+	@SystemService
+	LocationManager locationManager;
+
+	private MyLocationOverlay myLocationOverlay;
+
+	private MapController mapController;
+
+	private AddressOverlay addressOverlay;
+
 	@AfterViews
 	void initLayout() {
 		addressEditText.addTextChangedListener(new AbstractTextWatcher() {
@@ -39,6 +71,83 @@ public class AddressActivity extends MapActivity {
 				}
 			}
 		});
+		addressEditText.setOnEditorActionListener(new OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+					searchButtonClicked();
+					return true;
+				}
+				return false;
+			}
+		});
+
+		List<Overlay> mapOverlays = mapView.getOverlays();
+
+		GestureOverlay gestureOverlay = new GestureOverlay(this, new AbstractGestureListener() {
+			@Override
+			public void onLongPress(MotionEvent e) {
+				Projection projection = mapView.getProjection();
+				GeoPoint location = projection.fromPixels((int) e.getX(), (int) e.getY());
+				showAddressPopup(location);
+			}
+
+			@Override
+			public boolean onSingleTapUp(MotionEvent e) {
+				float x = e.getX();
+				float y = e.getY();
+				boolean addressTapped = addressOverlay.onSingleTapUp(x, y);
+				if (addressTapped) {
+					GeoPoint addressLocation = addressOverlay.getAddressLocation();
+					noteAddress(addressLocation);
+				} else {
+					Projection projection = mapView.getProjection();
+					GeoPoint location = projection.fromPixels((int) x, (int) y);
+					boolean myLocationTapped = myLocationOverlay.onTap(location, mapView);
+					if (!myLocationTapped) {
+						addressOverlay.hideAddressPopup();
+					}
+				}
+				return true;
+			}
+		});
+
+		myLocationOverlay = new MyLocationOverlay(this, mapView) {
+			@Override
+			protected boolean dispatchTap() {
+				GeoPoint myLocation = getMyLocation();
+				showAddressPopup(myLocation);
+				return true;
+			}
+		};
+
+		addressOverlay = new AddressOverlay();
+
+		mapOverlays.add(myLocationOverlay);
+		mapOverlays.add(addressOverlay);
+		mapOverlays.add(gestureOverlay);
+
+		mapController = mapView.getController();
+		mapController.setZoom(18);
+		mapView.setBuiltInZoomControls(true);
+
+		/*
+		 * Init location
+		 */
+
+		Criteria criteria = new Criteria();
+		String provider = locationManager.getBestProvider(criteria, true);
+		Location lastKnownLocation = locationManager.getLastKnownLocation(provider);
+
+		if (lastKnownLocation != null) {
+			int lastKnownLatitude = (int) (lastKnownLocation.getLatitude() * 1E6);
+			int lastKnownLongitude = (int) (lastKnownLocation.getLongitude() * 1E6);
+			GeoPoint initGeoPoint = new GeoPoint(lastKnownLatitude, lastKnownLongitude);
+			mapController.animateTo(initGeoPoint);
+		}
+
+		moveToMyLocationOnFirstFix();
 	}
 
 	protected void onAddressEmpty() {
@@ -51,9 +160,70 @@ public class AddressActivity extends MapActivity {
 		addressHint.setVisibility(View.GONE);
 	}
 
+	@Click
+	void searchButtonClicked() {
+		addressOverlay.hideAddressPopup();
+		inputMethodManager.hideSoftInputFromWindow(addressEditText.getWindowToken(), 0);
+		String address = addressEditText.getText().toString();
+		Toast.makeText(this, "MOCK Point for " + address, Toast.LENGTH_SHORT).show();
+		GeoPoint mockLocation = new GeoPoint(48831320, 2356224);
+		showAddressPopup(mockLocation);
+	}
+
+	private void showAddressPopup(GeoPoint location) {
+		addressOverlay.showAddressPopup(location);
+		mapController.animateTo(location);
+	}
+
+	@Click
+	void locationButtonClicked() {
+		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			moveToMyLocation();
+		} else {
+			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			startActivity(intent);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		myLocationOverlay.enableMyLocation();
+		myLocationOverlay.enableCompass();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		myLocationOverlay.disableMyLocation();
+		myLocationOverlay.disableCompass();
+	}
+
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
 
+	private void moveToMyLocation() {
+		GeoPoint currentLocation = myLocationOverlay.getMyLocation();
+		if (currentLocation != null) {
+			mapView.getController().animateTo(currentLocation);
+		} else {
+			moveToMyLocationOnFirstFix();
+			Toast.makeText(this, R.string.searching_location, Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private void moveToMyLocationOnFirstFix() {
+		myLocationOverlay.runOnFirstFix(new Runnable() {
+			public void run() {
+				moveToMyLocation();
+			}
+		});
+	}
+	
+	protected void noteAddress(GeoPoint location) {
+		addressOverlay.hideAddressPopup();
+		String mockAddress = "42 rue des petits indiens, 75006 Paris";
+	}
 }
---
+++ src/com/project202/AddressOverlay.java
@@ -0,0 +1,85 @@
+package com.project202;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Point;
+import android.graphics.PointF;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
+
+public class AddressOverlay extends Overlay {
+
+	private static final int CIRCLE_SIZE = 30;
+
+	private GeoPoint addressLocation;
+
+	private final Point addressPoint = new Point();
+	
+	private final Paint testPaint = new Paint();
+	private final Paint testPaint2 = new Paint();
+	private final Paint testPaint3 = new Paint();
+
+	public AddressOverlay() {
+		testPaint.setColor(Color.argb(100, 255, 0, 0));
+		testPaint2.setAntiAlias(true);
+		testPaint2.setColor(Color.rgb(0, 0, 0));
+		testPaint2.setStyle(Style.STROKE);
+		testPaint3.setAntiAlias(true);
+		testPaint3.setColor(Color.rgb(0, 0, 0));
+	}
+
+	public void showAddressPopup(GeoPoint location) {
+		this.addressLocation = location;
+
+	}
+
+	public void hideAddressPopup() {
+		addressLocation = null;
+	}
+
+	@Override
+	public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
+
+		if (addressLocation == null) {
+			return false;
+		}
+
+		if (shadow) {
+			return false;
+		}
+		
+		Projection projection = mapView.getProjection();
+		projection.toPixels(addressLocation, addressPoint);
+		
+		canvas.drawCircle(addressPoint.x, addressPoint.y, CIRCLE_SIZE, testPaint);
+		canvas.drawCircle(addressPoint.x, addressPoint.y, CIRCLE_SIZE, testPaint2);
+		canvas.drawCircle(addressPoint.x, addressPoint.y, 4, testPaint3);
+		
+		return false;
+	}
+
+	/**
+	 * Return true if popup was clicked
+	 */
+	public boolean onSingleTapUp(float x, float y) {
+		if (addressLocation == null) {
+			return false;
+		}
+		
+		// TODO : no tap if address being loaded
+		
+		float distance = PointF.length(x - addressPoint.x , y - addressPoint.y);
+		
+		return distance < CIRCLE_SIZE;
+	}
+
+	public GeoPoint getAddressLocation() {
+		return addressLocation;
+	}
+	
+}
