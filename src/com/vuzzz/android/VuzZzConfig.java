5f48d31bd1f0115bf50c10f62401f7ff31a72b7e
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-02-26 18:07:04 +0100

    Pre ICS design compatibility. Fixes #6.

--- src/com/vuzzz/android/VuzZzConfig.java
+++ src/com/vuzzz/android/VuzZzConfig.java
@@ -2,7 +2,7 @@ package com.vuzzz.android;
 
 public abstract class VuzZzConfig {
 	
-	public static final boolean RELEASE = true;
+	public static final boolean RELEASE = false;
 	
 	public static final boolean LOG_TO_CONSOLE = true && !RELEASE;
 	
